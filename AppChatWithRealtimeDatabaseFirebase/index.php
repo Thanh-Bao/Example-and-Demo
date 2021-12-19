7bbf1f5c6c7278038633004f5f4c71a3ef805645
Bảo Bảo <44540022+Thanh-Bao@users.noreply.github.com>
2021-12-19 21:23:52 +0700

    Update index.php

--- AppChatWithRealtimeDatabaseFirebase/index.php
+++ AppChatWithRealtimeDatabaseFirebase/index.php
@@ -1,40 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
 <script src="https://www.gstatic.com/firebasejs/8.8.0/firebase-app.js"></script>
 <script src="https://www.gstatic.com/firebasejs/8.8.0/firebase-database.js"></script>
 <script>
-  var firebaseConfig = {
-    apiKey: "AIzaSyDcsbxR0BZDaHyiKbvBgu5jNKOFAQoBS00",
-    authDomain: "sgfdsfsdf-f6c53.firebaseapp.com",
-    databaseURL: "https://sgfdsfsdf-f6c53-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "sgfdsfsdf-f6c53",
-    storageBucket: "sgfdsfsdf-f6c53.appspot.com",
-    messagingSenderId: "258739330034",
-    appId: "1:258739330034:web:d5fd883091bb2566dd696b"
-  };
-  firebase.initializeApp(firebaseConfig);
-  var myName = prompt("Nhập tên");
+    // FIREBASE REALTIME DATABASE 
+
+    // rule in console dasboard firebase
+    //     {
+    //   "rules": {
+    //     ".read": true,
+    //     ".write": true
+    //   }
+    // }
+    var firebaseConfig = {
+        databaseURL: "https://test-c1b1e-default-rtdb.asia-southeast1.firebasedatabase.app",
+    };
+    firebase.initializeApp(firebaseConfig);
 </script>
-<h1> Bảo Bảo Chat - app chat bảo mật - an toàn nhất thế giới :)))) </h1>
 <form onsubmit="return sendMessage();">
-    <input id="message" placeholder="Nhập tin nhắn" autocomplete="off">
+    <input id="myName" placeholder="name?" autocomplete="off">
+    <input id="message" placeholder="message?" autocomplete="off">
     <input type="submit">
 </form>
 <script>
     function sendMessage() {
-        var message = document.getElementById("message").value;
         firebase.database().ref("messages").push().set({
-            "sender": myName,
-            "message": message
+            "sender": document.getElementById("myName").value,
+            "message": document.getElementById("message").value
         });
         return false;
     }
 </script>
-<ul id="messages"></ul>
+<ul id="messagesList"></ul>
 <script>
-    firebase.database().ref("messages").on("child_added", function (snapshot) {
-        var html = "";
-        html += "<li id='message-" + snapshot.key + "'>";
+    firebase.database().ref("messages").on("child_added", function(snapshot) {
+        html = "<li>";
         html += snapshot.val().sender + ": " + snapshot.val().message;
         html += "</li>";
-        document.getElementById("messages").innerHTML += html;
+        document.getElementById("messagesList").innerHTML += html;
     });
 </script>
+
+</html>
